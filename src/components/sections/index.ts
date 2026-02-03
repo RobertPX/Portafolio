466107f40348d6c221d18de4be9ba7981fb6ab1b
RobertPX <lord.santoryu@gmail.com>
2026-02-03 11:04:58 -0400

    chore: add sections barrel export

---
+++ src/components/sections/index.ts
@@ -0,0 +1,6 @@
+export { Hero } from "./hero";
+export { Projects } from "./projects";
+export { TechStack } from "./tech-stack";
+export { About } from "./about";
+export { Contact } from "./contact";
+export { Footer } from "./footer";
